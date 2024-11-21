ac1c5e451960cbfdff9fa88f8427e9267240d007
lucas <lucas@Lucas>
2024-11-20 20:19:37 -0400

    Carpeta Base Proyecto

---
+++ src/main/java/com/mycompany/proyecto2/Proyecto2.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.proyecto2;
+import javax.swing.JOptionPane;
+/**
+ *
+ * @author erick
+ */
+public class Proyecto2 {
+
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog();
+    }
+}
